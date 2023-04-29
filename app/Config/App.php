92b28aee463b01fe60511595cc6559775384e3fb
SiguiB <99350925+SiguiB@users.noreply.github.com>
2023-04-28 23:07:18 -0700

    config

--- app/Config/App.php
+++ app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost/CRUDci/api';
+    public $baseURL = 'http://localhost/CRUDci/public';
 
     /**
      * --------------------------------------------------------------------------
