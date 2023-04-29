ba31e5b81ae4351478fcf95cecd3e405545ecacb
SiguiB <99350925+SiguiB@users.noreply.github.com>
2023-04-28 21:45:36 -0700

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost/codeigniter-proy/public/';
+    public $baseURL = 'http://localhost/CRUDci/public/';
 
     /**
      * --------------------------------------------------------------------------
